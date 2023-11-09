66776edda696b6b315079f7eed9f0f23d2dad380
Scott Taylor <s.taylor06@gmail.com>
2023-11-09 10:39:18 -0700

    qa changes and fixes to linting errors when run build failed'

--- Components/SliderImageGallery/script.js
+++ Components/SliderImageGallery/script.js
@@ -44,7 +44,7 @@ function initSliders (refs, data) {
     a11y: options.a11y,
     controller: {
       control: swiperSliderThumb
-    },
+    }
   })
 
   swiperSliderThumb.controller.control = swiperSliderMain
