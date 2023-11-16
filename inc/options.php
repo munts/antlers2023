51645d267ae2ca752bd17e72f3ea3644f6cf0915
Scott Taylor <s.taylor06@gmail.com>
2023-11-16 11:19:01 -0700

    gmaps api field in global options, mobile styling for homepage video, footer...

--- inc/options.php
+++ inc/options.php
@@ -12,3 +12,28 @@ add_filter('Flynt/addComponentData', function ($data, $componentName) {
     // Don’t overwrite existing data.
     return array_merge($options, $data);
 }, 9, 2);
+
+add_filter('acf/fields/google_map/api', function ($api) {
+    $apiKey = Options::getGlobal('Options', 'googleMapsApiKey');
+    if ($apiKey) {
+        $api['key'] = $apiKey;
+    }
+    return $api;
+});
+
+Options::addGlobal('Acf', [
+    [
+        'name' => 'googleMapsTab',
+        'label' => __('Google Maps', 'flynt'),
+        'type' => 'tab'
+    ],
+    [
+        'name' => 'googleMapsApiKey',
+        'label' => __('Google Maps Api Key', 'flynt'),
+        'type' => 'text',
+        'maxlength' => 100,
+        'prepend' => '',
+        'append' => '',
+        'placeholder' => ''
+    ]
+]);
