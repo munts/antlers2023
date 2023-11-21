cc9dfb98ee175d3bf3fcb1d51119a0f6839476ce
Scott Taylor <s.taylor06@gmail.com>
2023-11-21 08:50:41 -0700

    trying to resolve console errors - uncaught proimise or reading null of...

--- Components/BlockVideoOembed/script.js
+++ Components/BlockVideoOembed/script.js
@@ -5,16 +5,16 @@ export default function (el) {
     iframe: 'iframe'
   })
 
-  refs.playButton.addEventListener('click', loadVideo, { once: true })
+  // refs.playButton.addEventListener('click', loadVideo, { once: true })
 
-  function loadVideo () {
-    refs.iframe.addEventListener('load', videoIsLoaded, { once: true })
-    refs.iframe.setAttribute('src', refs.iframe.getAttribute('data-src'))
-    refs.videoPlayer.dataset.state = 'isLoading'
-  }
+  // function loadVideo () {
+  //   refs.iframe.addEventListener('load', videoIsLoaded, { once: true })
+  //   refs.iframe.setAttribute('src', refs.iframe.getAttribute('data-src'))
+  //   refs.videoPlayer.dataset.state = 'isLoading'
+  // }
 
-  function videoIsLoaded () {
-    refs.videoPlayer.dataset.state = 'isLoaded'
-    refs.posterImage.dataset.state = 'isHidden'
-  }
+  // function videoIsLoaded () {
+  //   refs.videoPlayer.dataset.state = 'isLoaded'
+  //   refs.posterImage.dataset.state = 'isHidden'
+  // }
 }
